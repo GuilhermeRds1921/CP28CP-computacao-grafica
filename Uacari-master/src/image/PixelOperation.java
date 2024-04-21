62759a2aae6503b0f8fb1a54596ce04873365893
GuilhermeRds1921 <guilhermerodrigues1921@gmail.com>
2024-04-21 15:09:40 -0300

    slides and first project

---
+++ Uacari-master/src/image/PixelOperation.java
@@ -0,0 +1,12 @@
+package image;
+
+public interface PixelOperation {
+
+	/**
+	 * Pixels of the image in ascending order.
+	 * @param p
+	 * @return
+	 */
+	public double compute(double... p);
+	
+}
